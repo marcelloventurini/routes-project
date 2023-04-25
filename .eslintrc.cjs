2c2cd56cc733fa4ef9866e4c98ea4be212f3214a
Marcello Venturini <marcellofventurini@gmail.com>
2023-04-25 17:28:08 -0300

    First commit.

---
+++ .eslintrc.cjs
@@ -0,0 +1,44 @@
+module.exports = {
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+  ],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: { react: { version: '18.2' } },
+  plugins: ['react-refresh'],
+  rules: {
+    'indent': [
+      'error',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'never'
+    ],
+    'jsx-quotes': [
+      'error',
+      'prefer-single'
+    ],
+    'no-multiple-empty-lines': [
+      'error',
+      {'max': 1}
+    ],
+    'padding-line-between-statements': [
+      'error',
+      {'blankLine': 'always', 'prev': '*', 'next': 'return'},
+      {'blankLine': 'always', 'prev': 'block-like', 'next': '*'}
+    ],
+    'react-refresh/only-export-components': 'warn',
+  },
+}
